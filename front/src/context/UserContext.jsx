8ed79106712941fab553b13edd06a957c6ab4d19
Dildar05 <noraacc09@gmail.com>
2024-12-11 17:32:15 +0500

    git comiit

--- front/src/context/UserContext.jsx
+++ front/src/context/UserContext.jsx
@@ -22,7 +22,7 @@ export const UserProvider = ({ children }) => {
           const userData = await userResponse.json();
           setUser(userData); // Сохраняем данные о пользователе в state
           // Запрос на получение целей пользователя
-          const goalsResponse = await fetch(`${pathUrl}/users/1/goals`);
+          const goalsResponse = await fetch(`${pathUrl}/users/${userId}/goals`);
           if (goalsResponse.ok) {
             const goalsData = await goalsResponse.json();
             console.log(goalsData);
