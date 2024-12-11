2768c220c29bd5df6c05906f20d0f7979633b7df
Leo_hsa <public@gmail.com>
2024-12-11 17:21:49 +0500

    fix path

--- front/src/pages/SignIn.jsx
+++ front/src/pages/SignIn.jsx
@@ -23,7 +23,7 @@ const SignIn = () => {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch('http://172.20.10.4:8000/api/v0/auth/login', {
+      const response = await fetch('http://localhost:8000/api/v0/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
